01138cc9a24232aff4e4e5427fdacb9f5a453e87
EdsonSoumay <s21810088@student.unklab.ac.id>
2023-10-09 13:06:50 +0900

    testing

--- index.js
+++ index.js
@@ -1,68 +1,92 @@
+// const express = require('express');
+// const bodyParser = require('body-parser')
+// const mongoose = require('mongoose')
+// const path = require('path')
+// const dotenv = require('dotenv').config();
+// const methodOverride = require('method-override') // untuk menghandle put
+// const connectDB = require('./src/config/db');
+
+// var createError = require('http-errors');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const session = require('express-session');
+// const flash = require('connect-flash');
+// const PORT = process.env.PORT;
+
+// const app = express();
+// const adminRouter = require('./src/routes/admin')
+// var apiRouter = require('./src/routes/api')
+
+// app.use(bodyParser.json()) // type json yang nantinya akan diterima
+
+// // untuk handle siapa sja yang dapat mengakses API kita
+// app.use((req, res, next)=>{
+//     res.setHeader('Access-Control-Allow-Origin','*') // Origin = url yang ingin di berikan akses API 
+//     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS') // method = method dalam penggunaan API 
+//     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
+//     next(); // agar requestnya tidak berhenti sampai disitu
+// })
+
+// // mongoose.set('strictQuery', false);
+// // mongoose.connect('mongodb://localhost:27017/db-onlineattendance')
+
+// connectDB();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, './src/views'));
+// app.set('view engine', 'ejs');
+// app.use(methodOverride('_method'));
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: true,
+//   saveUninitialized: true,
+//   cookie: { maxAge : 600000000}
+// }))
+// app.use(flash())
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))); // untuk mengarahkan ke path direktori sb-admin-2
+
+// app.get("/", (req, res) => res.send("Welcome to the API!"));
+
+// //admin
+// app.use('/admin', adminRouter)
+// //api
+// // app.use('/api/v1/member', apiRouter)
+// app.use('/user/api/v1', apiRouter); 
+
+// app.use((error, req, res, next)=>{
+//     const status = error.errorStatus || 500;
+//     const message = error.message;
+//     const data = error.data;
+//     res.status(status).json({message: message, data: data})
+// })
+
+// app.listen(PORT, ()=>console.log(`Conection Success from PORT ${PORT}`));
+
+
 const express = require('express');
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const path = require('path')
-const dotenv = require('dotenv').config();
-const methodOverride = require('method-override') // untuk menghandle put
-const connectDB = require('./src/config/db');
-
-var createError = require('http-errors');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const session = require('express-session');
-const flash = require('connect-flash');
-const PORT = process.env.PORT;
-
 const app = express();
-const adminRouter = require('./src/routes/admin')
-var apiRouter = require('./src/routes/api')
+const port = 3000;
 
-app.use(bodyParser.json()) // type json yang nantinya akan diterima
+// Route untuk halaman utama
+app.get('/', (req, res) => {
+  res.send('Selamat datang di server Express sederhana!');
+});
 
-// untuk handle siapa sja yang dapat mengakses API kita
-app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*') // Origin = url yang ingin di berikan akses API 
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS') // method = method dalam penggunaan API 
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
-    next(); // agar requestnya tidak berhenti sampai disitu
-})
+// Route lainnya
+app.get('/about', (req, res) => {
+  res.send('Ini adalah halaman "Tentang Kami".');
+});
 
-// mongoose.set('strictQuery', false);
-// mongoose.connect('mongodb://localhost:27017/db-onlineattendance')
+app.get('/kontak', (req, res) => {
+  res.send('Ini adalah halaman "Kontak".');
+});
 
-connectDB();
-
-// view engine setup
-app.set('views', path.join(__dirname, './src/views'));
-app.set('view engine', 'ejs');
-app.use(methodOverride('_method'));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { maxAge : 600000000}
-}))
-app.use(flash())
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))); // untuk mengarahkan ke path direktori sb-admin-2
-
-app.get("/", (req, res) => res.send("Welcome to the API!"));
-
-//admin
-app.use('/admin', adminRouter)
-//api
-// app.use('/api/v1/member', apiRouter)
-app.use('/user/api/v1', apiRouter); 
-
-app.use((error, req, res, next)=>{
-    const status = error.errorStatus || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({message: message, data: data})
-})
-
-app.listen(PORT, ()=>console.log(`Conection Success from PORT ${PORT}`));
+// Menjalankan server
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
